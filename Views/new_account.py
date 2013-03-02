ff3133a6242d0570207196a5716625731dacc47e
xray7224 <xray7224@googlemail.com>
2013-03-02 14:40:19 +0000

    adds mock new account view

--- Views/new_account.py
+++ Views/new_account.py
@@ -12,18 +12,31 @@ class NewAccount():
         self.loop = urwid.MainLoop(self.background, self.pallet, unhandled_input=self.input_handler)
 
         # ask for the webfinger
-        self.question = urwid.Edit(('bg', 'Webfinger ID: '))
-        self.blah = urwid.Text('Your webfinger ID is: ...')
-        self.loop.widget = urwid.AttrMap(self.background, 'bg')
-        self.loop.widget.original_widget = self.question
+        self.box = urwid.Pile([])
+        question = urwid.Edit(('bg', 'Webfinger ID: ',))
+        self.box.contents.append((question, self.box.options()))
+        
+        self.box.contents.append((urwid.Divider(), self.box.options()))
 
-        urwid.connect_signal(self.question, 'change', self.webfinger)
+        output = urwid.Text('Your webfinger ID is: ...')
+        self.box.contents.append((output, self.box.options()))
+        
+        self.padding = urwid.Padding(self.box, left=20, right=20)
+
+        self.loop.widget = urwid.AttrMap(self.background, 'bg')
+        self.loop.widget.original_widget = urwid.Filler(self.padding, 'top')
+        
+        urwid.connect_signal(question, 'change', self.webfinger)
+        self.webfinger_buff = ""
 
     def run(self):
         self.loop.run() 
 
     def webfinger(self, edit, webfinger):
-        self.blah.set_text("Your webfinger ID is: %s" % webfinger)
+        if webfinger == "enter":
+            self.box.contents[1][0].set_text("Your webfinger ID is: %s" % self.webfinger_buff)
+        else:
+            self.webfinger_buff += webfinger
 
     def input_handler(self, key):
         """ Handles input """
